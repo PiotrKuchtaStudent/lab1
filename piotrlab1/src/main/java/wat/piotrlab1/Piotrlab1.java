0782db5a13a74c91067b890a1c11c6a95cb93c5b
PiotrKuchtaStudent <151084604+PiotrKuchtaStudent@users.noreply.github.com>
2023-11-16 21:42:39 +0100

    Add files via upload

---
+++ piotrlab1/src/main/java/wat/piotrlab1/Piotrlab1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package wat.piotrlab1;
+
+/**
+ *
+ * @author piotr
+ */
+public class Piotrlab1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        new Okno1().setVisible(true);
+        
+    }
+}
